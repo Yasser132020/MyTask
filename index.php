b3ff9c50a9fc9087cb14a07a3614633a1d2e2ddf
Yasser132020 <62585174+Yasser132020@users.noreply.github.com>
2021-11-08 20:46:57 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+     function getChar($char){ 
+
+        $newChar = ord($char)+1;
+        #--- 90 & 122 are ascii values for 'z' & 'Z'
+        if($newChar === 91 || $newChar === 123){ 
+            $newChar -= 26;     #-- according to ascii values
+            return chr($newChar);
+        }else{
+            return chr($newChar);
+        }
+        
+    }
+
+    echo getChar('A');
+?>
